f133df4730309b8875051a24efc48008c6be1636
Gaurav Kumar <103420886+gauravai2025@users.noreply.github.com>
2025-01-18 11:23:20 +0530

    Update BikeTrainReducer.scala

--- code/MR_and_Spark/bike-sharing/src/main/java/BikeTrainReducer.scala
+++ code/MR_and_Spark/bike-sharing/src/main/java/BikeTrainReducer.scala
@@ -1,11 +1,6 @@
 import org.apache.hadoop.io.{IntWritable, Text}
 import org.apache.hadoop.mapreduce.Reducer
 
-
-/**
- * @author Alfred
- * @since 2023/11/17 22:33
- */
 class BikeTrainReducer extends Reducer[Text, IntWritable, Text, IntWritable] {
   override
   def reduce(key: Text, values: java.lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
